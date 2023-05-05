f596f78bf4f6cdb14f187eb1ee4d1c2fe8765a67
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-05-05 19:03:36 +0300

    add AboutUs section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Navbar from './components/Navbar/Navbar';
 import Header from 'sections/Header/Header';
+import AboutUs from 'sections/AboutUs/AboutUs';
 import './App.css';
 
 const App = () => (
   <div>
     <Navbar />
     <Header />
+    <AboutUs />
   </div>
 );
 
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,11 +1,15 @@
 import gericht from '../assets/gericht.png';
 import bg from '../assets/bg.png';
+import G from '../assets/G.png';
+import knife from '../assets/knife.png';
 import spoon from '../assets/spoon.png';
 import welcome from '../assets/welcome.png';
 
 const images = {
   bg,
+  G,
   gericht,
+  knife,
   spoon,
   welcome,
 };

13af321f2d2d05088eb0c3bed1e0d9511bbdf14c
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-05-05 18:29:40 +0300

    add Header section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Navbar from './components/Navbar/Navbar';
+import Header from 'sections/Header/Header';
 import './App.css';
 
 const App = () => (
   <div>
     <Navbar />
+    <Header />
   </div>
 );
 
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,13 @@
 import gericht from '../assets/gericht.png';
 import bg from '../assets/bg.png';
+import spoon from '../assets/spoon.png';
+import welcome from '../assets/welcome.png';
 
 const images = {
   bg,
   gericht,
+  spoon,
+  welcome,
 };
 
 export default images;
